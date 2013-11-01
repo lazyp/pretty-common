7969753df3783101fda8b04715e5356c58e304df
lazy_p <hexinglunlove@163.com>
2013-11-01 11:13:48 +0800

    README.md

---
+++ pretty-common-utils/src/test/java/org/pretty/common/utils/StringUtilsTest.java
@@ -0,0 +1,25 @@
+/**
+ *Copyright 2012-2013.All Rights Reserved
+ */
+package org.pretty.common.utils;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+/**
+ * @author <a href="mailto:lazy_p@163.com">lazyp</a>
+ * @version 2013-10-30
+ */
+public final class StringUtilsTest {
+    @Test
+    public void test() {
+        Assert.assertEquals(StringUtils.trimToNull("  "), null);
+        Assert.assertEquals(StringUtils.trim("  "), "");
+        Assert.assertEquals(StringUtils.trim(" abc   "), "abc");
+        Assert.assertEquals(StringUtils.isBlank(" "), true);
+        Assert.assertEquals(StringUtils.isBlank(null), true);
+        Assert.assertEquals(StringUtils.isNotBlank(" "), false);
+        Assert.assertEquals(StringUtils.isNotBlank("abc"), true);
+    }
+}
