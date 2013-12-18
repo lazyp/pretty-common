1a131957607444f84edf50c7ee3eebc690da038e
լÄÐСºÎ <hexinglun@gmail.com>
2013-12-18 13:40:03 +0800

    从HtmlUtils.java中移除extractImgSrc方法

--- pretty-common-utils/src/main/java/org/pretty/common/utils/HtmlUtils.java
+++ pretty-common-utils/src/main/java/org/pretty/common/utils/HtmlUtils.java
@@ -3,33 +3,12 @@
  */
 package org.pretty.common.utils;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 /**
  * Html 操作相关的工具
  * @author <a href="mailto:lazy_p@163.com">lazyp</a>
  * @version 2013-10-15
  */
 public final class HtmlUtils {
-    private static final Pattern extractImgSrcPattern = Pattern.compile("<img[^>]*src=\"([^\"]+)\"[^>]*>");
-
-    /**
-     * 提取html代码中的image url地址
-     * @param html
-     * @return
-     */
-    public static List<String> extractImgSrc(String html) {
-        List<String> imgSrcList = new ArrayList<String>();
-        Matcher m = extractImgSrcPattern.matcher(html);
-        while (m.find()) {
-            imgSrcList.add(m.group(1));
-        }
-        return imgSrcList;
-    }
-
     /**
      * 移除htmlTag
      * @param html
@@ -41,11 +20,5 @@ public final class HtmlUtils {
         }
         html = html.replaceAll("<[^>]+>", "");
         return html;
-    }
-
-    public static void main(String[] args) {
-        String txt = "asadsadsad<img src=\"xxxx.jpg\" alt=\"\"/>dasdsadadsa\n\r<img src=\"xda.gif\" alt=\"x\" >";
-        System.out.println(HtmlUtils.extractImgSrc(txt));
-        System.out.println(HtmlUtils.removeHtmlTag(txt));
     }
 }
--- pretty-common-utils/src/test/java/org/pretty/common/utils/HtmlUtlsTest.java
+++ pretty-common-utils/src/test/java/org/pretty/common/utils/HtmlUtlsTest.java
@@ -3,8 +3,6 @@
  */
 package org.pretty.common.utils;
 
-import java.util.Arrays;
-
 import junit.framework.Assert;
 
 import org.junit.Test;
@@ -14,13 +12,6 @@ import org.junit.Test;
  * @version 2013-10-30
  */
 public final class HtmlUtlsTest {
-    @Test
-    public void extractImgSrcTest() {
-        String html = "<span>image Url:<img src=\"http://aaa.bbb.com/a.png\" /></span><img src=\"http://ccc.ddd.com/d.png\" />";
-        String[] ret = new String[] { "http://aaa.bbb.com/a.png", "http://ccc.ddd.com/d.png" };
-        Assert.assertEquals(HtmlUtils.extractImgSrc(html), Arrays.asList(ret));
-    }
-
     @Test
     public void removeHtmlTagTest() {
         String html = "<span class='class' style=\"color:red;\">你好!</span>";
