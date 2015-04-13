06aa9d5dd40a4318d9a39618305322de4512358c
lazyp <lazy_p@163.com>
2015-04-13 17:44:19 +0800

    add the pretty-common-constant module and the JFXFont.java class

---
+++ pretty-common-constant/src/main/java/org/pretty/common/constant/JFXFont.java
@@ -0,0 +1,42 @@
+package org.pretty.common.constant;
+
+import java.awt.GraphicsEnvironment;
+
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+
+/**
+ * javafx font
+ * 
+ * @author lazyp
+ * @version 1.0.0
+ */
+public final class JFXFont {
+    public static final Font ARIAL_FONT = Font.font("Arial", FontWeight.NORMAL, 14);
+    public static final Font TIMES_NEW_ROMAN_FONT = Font.font("Times New Roman", FontWeight.NORMAL, 14);
+    public static final Font CONSOLAS_FONT = Font.font("Consolas", FontWeight.NORMAL, 14);
+
+    public static final Font UBUNTU_FONT = Font.font("Ubuntu", FontWeight.NORMAL, 14);
+
+    public static final Font WEI_RUAN_YA_HEI_FONT = Font.font("微软雅黑", FontWeight.NORMAL, 14);
+    public static final Font SONG_TI_FONT = Font.font("宋体", FontWeight.NORMAL, 14);
+    public static final Font NEW_SONG_TI_FONT = Font.font("新宋体", FontWeight.NORMAL, 14);
+    public static final Font HEI_TI_FONT = Font.font("黑体", FontWeight.NORMAL, 14);
+    public static final Font KAI_TI_FONT = Font.font("楷体", FontWeight.NORMAL, 14);
+
+    public static final Font WEN_QUAN_YI_MICRO_HEI_FONT = Font.font("WenQuanYi Micro Hei", FontWeight.NORMAL, 14);
+
+//    public static void main(String[] args) {
+//        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+//
+//        String fontNames[] = ge.getAvailableFontFamilyNames();
+//
+//        // Iterate the font family names
+//
+//        for (int i = 0; i < fontNames.length; i++) {
+//
+//            System.out.println(fontNames[i]);
+//
+//        }
+//    }
+}
