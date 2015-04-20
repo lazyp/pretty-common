3f0a0b7541d616995387b757f3e773920ef08163
lazyp <lazy_p@163.com>
2015-04-20 09:57:16 +0800

    [pretty-common-constant] charset const

---
+++ pretty-common-constant/src/main/java/org/pretty/common/constant/CharsetConst.java
@@ -0,0 +1,17 @@
+package org.pretty.common.constant;
+
+import java.nio.charset.Charset;
+
+/**
+ * 
+ * @author lazyp
+ *
+ */
+public final class CharsetConst {
+    public static final Charset ISO_8859_1 = Charset.forName("ISO-8859-1");
+    public static final Charset UTF_8 = Charset.forName("UTF-8");
+    public static final Charset GBK = Charset.forName("GBK");
+    public static final Charset GB2312 = Charset.forName("GB2312");
+    public static final Charset GB18030 = Charset.forName("GB18030");
+    public static final Charset BIG5 = Charset.forName("Big5");
+}
